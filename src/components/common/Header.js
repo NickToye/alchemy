3ed1cde96a9dc762e5f19a24571b6c2c110efcf1
Nick Toye <nick@pixelsandcode.co.uk>
2018-05-04 09:14:12 +0100

    Tidied up navigation for Alchemy.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,37 +3,45 @@ import { Link, IndexLink } from 'react-router';
 
 const Header = () => {
   return (
-    <header className="c-header  u-padding-tiny u-padding-top-huge">
+    <header className="c-header">
       <div className="c-toolbar__mq u-padding-tiny">
         <small>Current Media Query: </small>
       </div>
-      <nav className="c-navigation">
-        <IndexLink to="/" className="u-padding-horizontal-small" activeClassName="active">
-          <img src={require('../../images/Alchemy@3x.png')} className="a-patch  a-patch--alchemy" />
-        </IndexLink>
-        <Link to="/colours" className="u-padding-horizontal-small" activeClassName="active">
+      <IndexLink to="/" className="u-padding-small a-logo" activeClassName="active">
+        <img src={require('../../images/Alchemy@3x.png')} className="a-patch  a-patch--alchemy" />
+      </IndexLink>
+      <nav className="c-navigation  o-flex  o-flex--row">
+        <Link to="/colours" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
           <img src={require('../../images/Colours@3x.png')} className="a-patch" />
+          <span>Colours</span>
         </Link>
-        <Link to="/typography" className="u-padding-horizontal-small" activeClassName="active">
+        <Link to="/typography" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
           <img src={require('../../images/Typography@3x.png')} className="a-patch" />
+          <span>Typography</span>
         </Link>
-        <Link to="/buttons" className="u-padding-horizontal-small" activeClassName="active">
+        <Link to="/buttons" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
           <img src={require('../../images/Buttons@3x.png')} className="a-patch" />
+          <span>Forms</span>
         </Link>
-        <Link to="/layout" className="u-padding-horizontal-small" activeClassName="active">
+        <Link to="/layout" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
           <img src={require('../../images/Layout@3x.png')} className="a-patch" />
+          <span>Layout</span>
         </Link>
-        <Link to="/animations" className="u-padding-horizontal-small" activeClassName="active">
+        <Link to="/animations" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
           <img src={require('../../images/Animation@3x.png')} className="a-patch" />
+          <span>Animations</span>
         </Link>
-        <Link to="/surfaces" className="u-padding-horizontal-small" activeClassName="active">
-          <img src={require('../../images/Animation@3x.png')} className="a-patch" />
+        <Link to="/surfaces" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
+          <img src={require('../../images/Surfaces@3x.png')} className="a-patch" />
+          <span>Surfaces</span>
         </Link>
-        <Link to="/components" className="u-padding-horizontal-small" activeClassName="active">
-          <img src={require('../../images/Animation@3x.png')} className="a-patch" />
+        <Link to="/components" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
+          <img src={require('../../images/Components@3x.png')} className="a-patch" />
+          <span>Components</span>
         </Link>
-        <Link to="/code" className="u-padding-horizontal-small" activeClassName="active">
+        <Link to="/code" className="u-padding-horizontal-small o-flex__item" activeClassName="active">
           <img src={require('../../images/Code@3x.png')} className="a-patch" />
+          <span>Code</span>
         </Link>
       </nav>
     </header>
